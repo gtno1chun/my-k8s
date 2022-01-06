ebcc466a7d1a8226fb964ec808f2bd2257cb0596
gtno1chun <gtno1chun@gmail.com>
2022-01-06 10:16:59 +0900

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,39 +1,28 @@
-variable login_approle_role_id {}
+# variable login_approle_role_id {}
 
-variable login_approle_secret_id {}
-
-
-provider "vault" {
-  auth_login {
-    path = "auth/approle/login"
-
-    parameters = {
-      role_id   = var.login_approle_role_id
-      secret_id = var.login_approle_secret_id
-    }
-  }
-}
-
-# output {
-  
-#   description = ""
-#   value       = role_id
-# }
-# output "node_groups" {
-#   description = ""
-#   value       = module.eks.node_groups
-# }
+# variable login_approle_secret_id {}
 
 
 # provider "vault" {
-#   address = var.vault_endpoint
 #   auth_login {
-#     path = "auth/aws/login"
-#     method = "aws"
-#     parameters = {
-#       role = "test-role"
-#       # heaer_value = "vault-token-test-role"
-#     }
+#     path = "auth/approle/login"
 
+#     parameters = {
+#       role_id   = var.login_approle_role_id
+#       secret_id = var.login_approle_secret_id
+#     }
 #   }
 # }
+
+provider "vault" {
+  address = var.vault_endpoint
+  auth_login {
+    path = "auth/aws/login"
+    method = "aws"
+    parameters = {
+      role = "test-role"
+      # heaer_value = "vault-token-test-role"
+    }
+
+  }
+}
