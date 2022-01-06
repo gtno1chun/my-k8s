172d0e0dabcb8ec502d50274e9ce0073565c083f
gtno1chun <gtno1chun@gmail.com>
2022-01-06 10:09:19 +0900

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,3 +1,36 @@
+variable login_approle_role_id {
+  description = ""
+  value   = "Token"
+}
+
+variable login_approle_secret_id {
+  description = ""
+  value   = "jackchun-token"
+}
+
+
+provider "vault" {
+  auth_login {
+    path = "auth/approle/login"
+
+    parameters = {
+      role_id   = var.login_approle_role_id
+      secret_id = var.login_approle_secret_id
+    }
+  }
+}
+
+# output {
+  
+#   description = ""
+#   value       = role_id
+# }
+# output "node_groups" {
+#   description = ""
+#   value       = module.eks.node_groups
+# }
+
+
 provider "vault" {
   address = var.vault_endpoint
   auth_login {
@@ -5,7 +38,7 @@ provider "vault" {
     method = "aws"
     parameters = {
       role = "test-role"
-      heaer_value = "vault-token-test-role"
+      # heaer_value = "vault-token-test-role"
     }
 
   }
