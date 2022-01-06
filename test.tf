6a5d931209a43f7343c53ee197d01cc70f0ba197
gtno1chun <gtno1chun@gmail.com>
2022-01-06 14:12:19 +0900

    Update test.tf

--- test.tf
+++ test.tf
@@ -106,7 +106,7 @@ variable "approle_secret_id" {
 provider "vault" {
   address = var.vault_endpoint 
   auth_login {
-    path = "auth/aws/login"
+    path = "auth/tfc/login"
     parameters = {
       role_id   = var.approle_id
       secret_id = var.approle_secret_id
