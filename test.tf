189a877228a839b68de7d42719e6883dc9556f07
gtno1chun <gtno1chun@gmail.com>
2022-01-06 15:59:46 +0900

    Update test.tf

--- test.tf
+++ test.tf
@@ -11,48 +11,22 @@ variable "VAULT_TOKEN" {
   
 }
 
+// vault provider는 write-only 값으로 주입된 환경변수 VAULT_TOKEN을 이용한다.
 provider "vault" {
-  address = var.vault_endpoint
-
-  # auth_login {
-  #   path    = "auth/token/login"
-  #   token = var.VAULT_TOKEN   
-  # } 
-  #alias = "approle"
-  # alias = "Token"
-  # token = "jackchun-token"  #"${vault_approle_auth_backend_login.login.client_token}"
+  address = "~"
 }
 
+terraform {
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "~"
 
-resource "vault_aws_secret_backend" "aws" {
-  access_key = var.approle_id
-  secret_key = var.approle_secret_id
-  path       = "${var.name}-path"
-
-
-}
-
-
-resource "vault_aws_secret_backend_role" "role" {
-  backend = vault_aws_secret_backend.aws.path
-  name            = "${var.name}-role"
-  credential_type = "iam_user"
-
-  policy_document = <<EOT
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Action": "ec2:*",
-      "Resource": "*"
+    workspaces {
+      name = "~"
     }
-  ]
-}
-EOT
+  }
 }
 
-# generally, these blocks would be in a different module
 data "vault_aws_access_credentials" "tfc" {
   backend = "aws"
   role    = "tfc"
@@ -64,12 +38,4 @@ provider "aws" {
   access_key = data.vault_aws_access_credentials.tfc.access_key
   secret_key = data.vault_aws_access_credentials.tfc.secret_key
   token      = data.vault_aws_access_credentials.tfc.security_token
-}
-
-output "backend" {
-  value = vault_aws_secret_backend.aws.path
-}
-
-output "role" {
-  value = vault_aws_secret_backend_role.role.name
 }
