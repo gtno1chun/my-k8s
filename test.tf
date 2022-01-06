10ffe4574bf6771a951d9c2f804ee3b07d44ab21
gtno1chun <gtno1chun@gmail.com>
2022-01-06 15:31:41 +0900

    Update test.tf

--- test.tf
+++ test.tf
@@ -22,9 +22,10 @@ resource "vault_aws_secret_backend" "aws" {
 
 resource "vault_aws_secret_backend_role" "role" {
   backend = vault_aws_secret_backend.aws.path
-  name    = "test"
+  name            = "${var.name}-role"
+  credential_type = "iam_user"
 
-  policy = <<EOT
+  policy_document = <<EOT
 {
   "Version": "2012-10-17",
   "Statement": [
