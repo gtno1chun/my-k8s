d2a9e2b4c341f48f510fbda6c52814a7b150480c
gtno1chun <gtno1chun@gmail.com>
2022-01-06 10:36:15 +0900

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -32,8 +32,8 @@ provider "vault" {
   }
 }
 
-variable vault_secrets_engine {}
-variable vault_access_role {}
+# variable vault_secrets_engine {}
+# variable vault_access_role {}
 
 data "vault_aws_access_credentials" "iam" {
   # region = var.env == "stg-cn" || var.env == "prod-cn" ? "cn-northwest-1" : "us-east-1"  ## china region workspace 는 반드시 cn-northwest-1 로 호출 하도록 해야 함
