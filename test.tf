22cca1c5522a4e75dc051b5d76ae8e11e6ba4e7e
gtno1chun <gtno1chun@gmail.com>
2022-01-06 13:26:57 +0900

    Update test.tf

--- test.tf
+++ test.tf
@@ -1,92 +1,108 @@
-variable "name" { default = "dynamic-aws-creds-operator" }
-variable "region" { default = "ap-northeast-2" }
-# variable "path" { default = "../vault-admin-workspace/terraform.tfstate" }
-variable "ttl" { default = "1" }
+# variable "name" { default = "dynamic-aws-creds-operator" }
+# variable "region" { default = "ap-northeast-2" }
+# # variable "path" { default = "../vault-admin-workspace/terraform.tfstate" }
+# variable "ttl" { default = "1" }
 
-# terraform {
-#   backend "local" {
-#     path = "terraform.tfstate"
+# # terraform {
+# #   backend "local" {
+# #     path = "terraform.tfstate"
+# #   }
+# # }
+
+# data "terraform_remote_state" "my-k8s" {
+#   backend = "local"
+
+#   config = {
+#     path = var.path
 #   }
 # }
 
-data "terraform_remote_state" "my-k8s" {
-  backend = "local"
+# # provider "vault" {
+# #   address = var.vault_endpoint
+# #   auth_login {
+# #     path = "auth/aws/login"
+# #     method = "aws"
+# #     parameters = {
+# #       role = "jackchun-role"
+# #       # heaer_value = "vault-token-test-role"
+# #     }
 
-  config = {
-    path = var.path
-  }
-}
+# #   }
+# # }
+
+# variable "approle_id" {
+#   description = ""
+
+# }
+# variable "approle_secret_id" {
+#   description = ""
+
+# }
+
 
 # provider "vault" {
 #   address = var.vault_endpoint
 #   auth_login {
-#     path = "auth/aws/login"
-#     method = "aws"
+#     path = "auth/tfc/login"
 #     parameters = {
-#       role = "jackchun-role"
-#       # heaer_value = "vault-token-test-role"
+#       role_id   = var.approle_id
+#       secret_id = var.approle_secret_id
 #     }
-
 #   }
 # }
 
-variable "approle_id" {
-  description = ""
 
-}
-variable "approle_secret_id" {
-  description = ""
+# data "vault_aws_access_credentials" "iam" {
+#   # backend = data.terraform_remote_state.my-k8s.outputs.backend
+#   # role    = data.terraform_remote_state.my-k8s.outputs.role
+#   backend = "aws-tfc" 
+#   role    = "data.terraform_remote_state.my-k8s.outputs.role
+# }
 
-}
+# provider "aws" {
+#   region     = var.region
+#   access_key = data.vault_aws_access_credentials.iam.access_key
+#   secret_key = data.vault_aws_access_credentials.iam.secret_key
+# }
 
+# data "aws_ami" "ubuntu" {
+#   most_recent = true
+
+#   filter {
+#     name   = "name"
+#     values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
+#   }
+
+#   filter {
+#     name   = "virtualization-type"
+#     values = ["hvm"]
+#   }
+
+#   owners = ["481230465846"] # Canonical
+# }
+
+# # Create AWS EC2 Instance
+# resource "aws_instance" "main" {
+#   ami           = data.aws_ami.ubuntu.id
+#   instance_type = "t2.nano"
+
+#   tags = {
+#     Name  = var.name
+#     TTL   = var.ttl
+#     owner = "${var.name}-guide"
+#   }
+# }
+
+
+variable login_username {}
+variable login_password {}
 
 provider "vault" {
-  address = var.vault_endpoint
   auth_login {
-    path = "auth/tfc/login"
+    path = "auth/userpass/login/jackchun"
+
     parameters = {
-      role_id   = var.approle_id
-      secret_id = var.approle_secret_id
+      password = "jackchun" #var.login_password
     }
-  }
-}
-
-
-data "vault_aws_access_credentials" "creds" {
-  backend = data.terraform_remote_state.my-k8s.outputs.backend
-  role    = data.terraform_remote_state.my-k8s.outputs.role
-}
-
-provider "aws" {
-  region     = var.region
-  access_key = data.vault_aws_access_credentials.creds.access_key
-  secret_key = data.vault_aws_access_credentials.creds.secret_key
-}
-
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["481230465846"] # Canonical
-}
-
-# Create AWS EC2 Instance
-resource "aws_instance" "main" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t2.nano"
-
-  tags = {
-    Name  = var.name
-    TTL   = var.ttl
-    owner = "${var.name}-guide"
   }
 }
