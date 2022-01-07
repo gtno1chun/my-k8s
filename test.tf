a31d54defb9a65156a59d5678a336ac37332ea10
gtno1chun <gtno1chun@gmail.com>
2022-01-07 17:42:46 +0900

    Update test.tf

--- test.tf
+++ test.tf
@@ -1,64 +1,3 @@
-# // aws/config/root에 입력될 user
-# resource "aws_iam_user" "tfc" {
-#   name     = "terraform-cloud"
-# }
-
-# resource "aws_iam_user_policy" "tfc" {
-#   name     = "terraform-cloud-policy"
-#   user     = aws_iam_user.tfc.name
-#   policy   = <<EOF
-# {
-#   "Version": "2012-10-17",
-#   "Statement": [
-#     {
-#       "Effect": "Allow",
-#       "Action": [
-#         "sts:AssumeRole"
-#       ],
-#       "Resource": [
-#         "arn:aws:iam::481230465846:role/TerraformCloud"
-#       ]
-#     }
-#   ]
-# }
-# EOF
-# }
-
-# resource "aws_iam_access_key" "tfc" {
-#   user     = aws_iam_user.tfc.name
-# }
-
-# // 실제로 TFC가 발급 받아서 사용하는 role
-# resource "aws_iam_role" "tfc" {
-#   name     = "TerraformCloud"
-
-#   assume_role_policy = <<EOF
-# {
-#   "Version": "2012-10-17",
-#   "Statement": [
-#     {
-#       "Effect": "Allow",
-#       "Principal": {
-#         "AWS": "arn:aws:iam::481230465846:user/terraform-cloud"
-#       },
-#       "Action": "sts:AssumeRole"
-#     }
-#   ]
-# }
-# EOF
-# }
-
-# // TFC가 사용하게될 policy, 꼭 AdministratorAccess일 필요는 없다.
-# resource "aws_iam_role_policy_attachment" "tfc" {
-#   role       = aws_iam_role.tfc.name
-#   policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
-# }
-
-variable "name" { default = "dynamic-aws-creds-operator" }
-variable "region" { default = "ap-northeast-2" }
-#variable "path" { default = "../vault-admin-workspace/terraform.tfstate" }
-variable "ttl" { default = "1" }
-
 data "aws_ami" "ubuntu" {
   most_recent = true
 
@@ -75,14 +14,11 @@ data "aws_ami" "ubuntu" {
   owners = ["481230465846"] # Canonical
 }
 
-# Create AWS EC2 Instance
-resource "aws_instance" "main" {
+resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
-  instance_type = "t2.nano"
+  instance_type = "t3.micro"
 
   tags = {
-    Name  = var.name
-    TTL   = var.ttl
-    owner = "${var.name}-guide"
+    Name = "HelloWorld"
   }
 }
