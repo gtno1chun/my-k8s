2078fbf90dba3adb2f04e7c85c9e0d51b53c38c9
gtno1chun <gtno1chun@gmail.com>
2022-01-10 13:23:02 +0900

    test add

---
+++ iam.tf
@@ -0,0 +1,54 @@
+resource "aws_iam_user" "tfc" {
+  name  = "terraform-cloud"
+}
+
+resource "aws_iam_user_policy" "tfc" {
+  name    = "terraform-cloud-policy" 
+  user    = aws_iam_user.tfc.name 
+  policy  = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "sts:AssumeRole"
+      ],
+      "Resource": [
+        "arn:aws:iam::481230465846:role/TerraformCloud"
+      ]
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_access_key" "tfc" {
+  user    = aws_iam_user.tfc.name 
+}
+
+// 실체 TFC가 발급 받아서 사용하는 role 
+resource "aws_iam_role" "tfc" {
+  name  = "TerraformCloud"
+
+  assum_role_policy = <<EOF 
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "arn:aws:iam::481230465846:user/terraform-cloud"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+// TFC가 사용하게될 policy, 꼭 AdministratorAccess일 필요는 없다.
+resource "aws_iam_role_policy_attachment" "tfc" {
+  role       = aws_iam_role.tfc.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
