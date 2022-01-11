a5390a24880ea3883634741621d1ff16e3980d46
gtno1chun <gtno1chun@gmail.com>
2022-01-11 16:17:32 +0900

    init

--- provider.tf
+++ provider.tf
@@ -30,7 +30,7 @@ provider "vault" {
 
 data "vault_aws_access_credentials" "vault-role" {
   backend = "aws"
-  role    = "vault-role"
+  role    = "TerraformCloud"
   #type    = "creds"
   #type    = "sts"
 }
