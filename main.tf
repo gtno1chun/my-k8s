09274af6f3cb0b7d5e5b233db3f73443b7e8aeb5
gtno1chun <gtno1chun@gmail.com>
2022-07-07 15:09:46 +0900

    eks cluster upgrade 1.21

--- main.tf
+++ main.tf
@@ -26,10 +26,10 @@ module "eks" {
   cluster_endpoint_public_access  = true
 
   vpc_id  = module.vpc.vpc_id
-  subnets_ids = module.vpc.private_subnets
+  subnet_ids = module.vpc.private_subnets
 
   # node_groups_defaults = {
-  eks_managed_node_group_efaults = {
+  eks_managed_node_group_defaults = {
     ami_type  = "AL2_x86_64"
     disk_size = 50
   }
